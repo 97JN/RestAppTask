a5ed7bd745544355d286ddd7b4ee3817aa5585ff
Jakub Nieradko <Jakub.Nieradko@ttsw.com.pl>
2023-03-06 08:12:26 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/controller/CarController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import com.example.demo.annotation.AnnotationClass;
+import com.example.demo.entity.CarEntity;
+import com.example.demo.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/cars")
+public class CarController {
+
+    private CarService carService;
+
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping("/all")
+    public Iterable <CarEntity> getAll(){
+        return carService.findAll();
+    }
+
+    @GetMapping
+    public Optional<CarEntity>getById(@RequestParam Integer id){
+        return carService.findById(id);
+    }
+
+    @DeleteMapping
+    public void deleteBy(@RequestParam Integer id){
+        carService.deletyById(id);
+    }
+
+    @PostMapping
+    @AnnotationClass
+    public CarEntity addCar(@RequestBody CarEntity carEntity){
+        return carService.save(carEntity);
+    }
+
+    @PutMapping
+    public CarEntity editCar(@RequestBody CarEntity carEntity){
+        return carService.save(carEntity);
+    }
+
+}
